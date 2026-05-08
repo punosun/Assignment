50668df4d10c8d34856ee8b29393ce043dec51af
Vattensork <lilleman11andersson@hotmail.com>
2026-05-08 11:49:09 +0200

    Update Book.cs

--- LibraryManagement/Book.cs
+++ LibraryManagement/Book.cs
@@ -2,5 +2,56 @@
 
 public class Book
 {
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
+{
+    internal class Book
+    {
+
+        public Book(string title, string author, string isbn, string status)
+        {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            this.status = status;
+        }
+
+        private string title;
+        private string author;
+        private string isbn;
+        private string status;
+
+        public string getTitle()
+
+        { return title; }
+    
+        public string getAuthor()
+
+        { return author; }
+       
+        public string getIsbn()
+        { return isbn; }
+        
+        public void GetDetails()
+        {
+            Console.WriteLine(" Title: " + title + ": " + " Author: " + author +  "  " + " ISBN: " + isbn + " ");
+        }
+
+        public void SetStatus() // Check if book is available or on loan
+        { 
+
+        }
+
+        
+    }
+
+
+}
 }
